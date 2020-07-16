0d67859afad0771b2166f8ceb4b286fdf4b14dba
pedrofelli <pedrofellipeunai@gmail.com>
2020-07-15 21:26:45 -0300

    criação do customer e produto

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -5,7 +5,13 @@ import CreateCustomerService from '@modules/customers/services/CreateCustomerSer
 import { container } from 'tsyringe';
 
 export default class CustomersController {
-  public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+  public async create(request: Request, response: Response): Promise<any> {
+    const { email, name } = request.body;
+
+    const createCustomerService = container.resolve(CreateCustomerService);
+
+    const customer = await createCustomerService.execute({ email, name });
+
+    return response.json(customer);
   }
 }
